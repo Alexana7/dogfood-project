e2173a1b022d66ee9f1fbb7aa3e6df66dc1fdf76
Alexana7 <ngayane@bk.ru>
2024-02-01 14:34:56 +0300

    add dayjs lib

--- src/components/review/index.jsx
+++ src/components/review/index.jsx
@@ -1,16 +1,17 @@
-import './styles.css';
+import s from './styles.module.css';
 import { Rating } from "../rating";
+import { formattedDate } from '../../utils/products';
 
 export function Review ({ author, city, text, created_at, rating, _id, photos }) {
   return ( 
-    <div className="review">
-      <div className="review__header">
-        <div className="review__name">{author.name}</div>
-        <div className="review__date">{created_at}</div>
+    <div className={s.review}>
+      <div className={s.review__header}>
+        <div className={s.review__name}>{author.name}</div>
+        <div className={s.review__date}>{formattedDate(created_at)}</div>
       </div>
       <Rating currentRating={rating} />
-      {city && <div className="review__city">{city}</div>}
-      <p className="review__text">'{text}'</p>
+      {city && <div className={s.review__city}>{city}</div>}
+      <p className={s.review__text}>'{text}'</p>
 
     </div>
    );
